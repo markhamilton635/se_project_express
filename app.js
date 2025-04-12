43b52a6faf3931d1f593c598263c8a05b78ea1ad
MARK HAMILTON <HAMILTON.MARK37@GMAIL.COM>
2025-04-12 11:57:23 -0700

    user schema added

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
-const express = require('express');
+const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
-const {PORT = 3001} = process.env;
+const { PORT = 3001 } = process.env;
+
+
+mongoose
+  .connect("mongodb://127.0.0.1:27017/wtwr_db")
+  .then(()=> {
+    console.log("Connected to DB");
+  })
+  .catch(console.error);
 
 
 app.listen(PORT, () => {
-  console.log(`App listening at port ${PORT}`)
-})
+  console.log(`Server is running on port ${PORT}`);
+});
