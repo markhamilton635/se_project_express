b847e5081aa7e5d6883af70978994ecc5e1632f0
MARK HAMILTON <HAMILTON.MARK37@GMAIL.COM>
2025-04-13 16:11:34 -0700

    correct errors

--- models/clothingitem.js
+++ models/clothingitem.js
@@ -23,9 +23,20 @@ const clothingItemSchema = new mongoose.Schema({
       message: "You must enter a valid URL",
     },
   },
-  // owner: {},
-  // likes: {},
-  // createdAt: {},
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  likes: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    default: []
+  }],
+  createdAt: {
+    type: Date,
+    default: Date.now
+  },
 });
 
 module.exports = mongoose.model("item", clothingItemSchema);
