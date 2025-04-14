756abef35905070747f2d47b8945b05cfb3962c8
MARK HAMILTON <HAMILTON.MARK37@GMAIL.COM>
2025-04-13 18:37:17 -0700

    updating gitactions

--- utils/errors.js
+++ utils/errors.js
@@ -10,19 +10,4 @@ module.exports = {
   UNAUTHORIZED,
   FORBIDDEN,
   SERVER_ERROR,
-  handleCatch
-}
-
-
-function handleCatch (err) {
-  console.error(err);
-  if (err.name === "ValidationError") {
-    res.status(BAD_REQUEST).send({ message: err.message });
-  } else if (err.name === "DocumentNotFoundError") {
-    res.status(NOT_FOUND).send({ message: err.message });
-  } else if (err.name === "CastError") {
-    res.status(NOT_FOUND).send({ message: err.message });
-  }
-
-  return res.status(SERVER_ERROR).send({ message: err.message });
 };
